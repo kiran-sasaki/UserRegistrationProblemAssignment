2f8f2519b13613572a098868d812d2d4a4ee8879
kiran-sasaki <kiransekar1111@gmail.com>
2022-12-02 15:47:57 +0530

    Added FirstName Validation Function For UserRegistration Problem

---
+++ UserRegistrationProblemAssignment/UserDetials.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblemAssignment
+{
+    public class UserDetials
+    {
+        //Regular Expression For FirstName Validation
+        public static string REGEX_FIRSTNAME = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public bool ValidateFirstname(string firstname)
+        {
+            return Regex.IsMatch(firstname, REGEX_FIRSTNAME);
+
+        }
+    }
+}
