cfc45c26d8298471d2484be833fe0c69c8664ca2
kiran-sasaki <kiransekar1111@gmail.com>
2022-12-03 16:17:31 +0530

    Added EmailAddress Validation Function For UserRegistration Problem

--- UserRegistrationProblemAssignment/UserDetials.cs
+++ UserRegistrationProblemAssignment/UserDetials.cs
@@ -13,6 +13,8 @@ namespace UserRegistrationProblemAssignment
         public static string REGEX_FIRSTNAME = "^[A-Z]{1}[A-Za-z]{2,}$";
         //Regular Expression For LastName Validation
         public static string REGEX_LASTNAME = "^[A-Z]{1}[A-Za-z]{2,}$";
+        //Regular Expression For Email Validation
+        public static string REGEX_EMAIL_ADDRESS = "^[a-zA-Z0-9]([._+-]{0,1}[a-zA-Z0-9])*[@]{1}[a-zA-Z0-9]{1,}[.]{1}[a-zA-Z]{2,3}([.]{1}[a-zA-Z]{2,3}){0,1}$";
         public bool ValidateFirstname(string firstname)
         {
             return Regex.IsMatch(firstname, REGEX_FIRSTNAME);
@@ -20,6 +22,10 @@ namespace UserRegistrationProblemAssignment
         public bool ValidateLastname(string lastname)
         {
             return Regex.IsMatch(lastname, REGEX_LASTNAME);
+        }
+        public bool validateEmailAddress(string emailAddress)
+        {
+            return Regex.IsMatch(emailAddress, REGEX_EMAIL_ADDRESS);
         }
     }
 }
